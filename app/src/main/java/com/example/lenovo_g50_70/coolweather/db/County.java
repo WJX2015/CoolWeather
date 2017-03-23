3b419381d862c289189a610370ab124cdf640469
吴俊兴 <550481089@qq.com>
2017-03-23 15:16:21 +0800

    第一阶段，加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/lenovo_g50_70/coolweather/db/County.java
@@ -0,0 +1,46 @@
+package com.example.lenovo_g50_70.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by lenovo-G50-70 on 2017/3/23.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+}
---
+++ app/src/main/java/com/example/lenovo_g50_70/coolweather/db/Province.java
@@ -0,0 +1,37 @@
+package com.example.lenovo_g50_70.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by lenovo-G50-70 on 2017/3/23.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
