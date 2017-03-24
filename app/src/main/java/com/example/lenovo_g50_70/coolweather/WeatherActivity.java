04ea65ba8185b1f113a5f601ebb03ffff3878cad
吴俊兴 <550481089@qq.com>
2017-03-24 21:21:31 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/com/example/lenovo_g50_70/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/lenovo_g50_70/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.lenovo_g50_70.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.lenovo_g50_70.coolweather.gson.Forecast;
 import com.example.lenovo_g50_70.coolweather.gson.Weather;
+import com.example.lenovo_g50_70.coolweather.service.AutoUpdateService;
 import com.example.lenovo_g50_70.coolweather.util.HttpUtil;
 import com.example.lenovo_g50_70.coolweather.util.Utility;
 
@@ -215,5 +217,7 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
